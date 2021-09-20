5edb4c4660c967d36e49f575100c17fc900ebb5c
mercy34mercy <etukobamasatyan@gmail.com>
2021-09-20 10:53:23 +0900

    firstcommit

---
+++ app.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'hello, world'
+
+@app.route('/hello')
+def indexs():
+    return 'hello'
+@app.route('/hello')
+def indexs():
+    return 'hello'
+
+
+
+if __name__ == '__main__':
+    app.run()
