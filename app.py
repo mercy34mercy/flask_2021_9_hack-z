8ad5124ce22edc1a4dd8f140c6392c22c79fff0b
mercy34mercy <etukobamasatyan@gmail.com>
2021-09-21 14:33:06 +0900

    かんせいした

--- app.py
+++ app.py
@@ -4,8 +4,10 @@ import requests
 from request import req
 from get_greate import great_img
 from flask import *
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def index():
