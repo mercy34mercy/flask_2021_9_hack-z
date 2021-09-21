439988b0f221a3447e080d302c1f5fedfa3d232b
mercy34mercy <etukobamasatyan@gmail.com>
2021-09-21 10:35:59 +0900

    firscommit

--- request.py
+++ request.py
@@ -5,7 +5,7 @@ from requests.api import get
 
 
 def req(qua,num):
-    json_t = requests.post("http://beautifulsoup.azurewebsites.net/post",{"quali":qua,"num":num})
+    json_t = requests.post("https://get-keys.herokuapp.com/url/post",{"quali":qua,"num":num})
 
     #json = json
     json_dict = json.loads(json_t.text)
